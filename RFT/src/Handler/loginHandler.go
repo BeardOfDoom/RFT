561b66aab0e11faa3602975815bc3b49823b3ff0
Gencsi Mátyás <gencsimatyas@webshopexperts.hu>
2016-11-27 20:03:31 +0100

    Login frontend fix

--- RFT/src/Handler/loginHandler.go
+++ RFT/src/Handler/loginHandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"Service"
+	"html/template"
 )
 
 func Authentificate(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +12,15 @@ func Authentificate(w http.ResponseWriter, r *http.Request) {
 
 	valid := Service.Authentificate(r.FormValue("username"), r.FormValue("password"))
 	fmt.Println(valid)
+
+	if(valid) {
+		data := "" //TODO: itt kell majd struktokkal átadni a bejelentkezéshez szükséges infókat
+		t, _ := template.ParseFiles("View/HomePage/index.html", "View/Layout/main.html")
+		t.ExecuteTemplate(w, "layout", data)
+	} else {
+		data := ""
+		t, _ := template.ParseFiles("View/Login/error.html", "View/Layout/main.html")
+		t.ExecuteTemplate(w, "layout", data)
+	}
+
 }
