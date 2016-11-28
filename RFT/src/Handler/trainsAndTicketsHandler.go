4f771b7aef1f6498b083720be03d7ab393f5430b
Gencsi Mátyás <gencsimatyas@webshopexperts.hu>
2016-11-28 22:02:59 +0100

    Pure English

--- RFT/src/Handler/trainsAndTicketsHandler.go
+++ RFT/src/Handler/trainsAndTicketsHandler.go
@@ -12,17 +12,17 @@ func SearchTimetable(w http.ResponseWriter, r *http.Request) {
 	//TODO: kitálni, hogy hogyan legyen lekezelve a discount és a pótjegy nélkül majd az ár kiszámításánál
 	//TODO: valamint a helyi közlekedés nélkül és a kerékpárszállítással is
 	result := Service.SearchTimetable(r.FormValue("from"), r.FormValue("to"), r.FormValue("date"),
-		r.FormValue("discount"), r.FormValue("potjegy"), r.FormValue("helyi"),
-		r.FormValue("atszallas"), r.FormValue("kerekpar"))
+		r.FormValue("discount"), r.FormValue("extraTicket"), r.FormValue("withoutLocalTransportation"),
+		r.FormValue("withoutChange"), r.FormValue("withBicycleDelivery"))
 	fmt.Println(result)
 	fmt.Println(r.FormValue("from"))
 	fmt.Println(r.FormValue("to"))
 	fmt.Println(r.FormValue("date"))
 	fmt.Println(r.FormValue("discount"))
-	fmt.Println(r.FormValue("potjegy"))
-	fmt.Println(r.FormValue("helyi"))
-	fmt.Println(r.FormValue("atszallas"))
-	fmt.Println(r.FormValue("kerekpar"))
+	fmt.Println(r.FormValue("withoutExtraTicket"))
+	fmt.Println(r.FormValue("withoutLocalTransportation"))
+	fmt.Println(r.FormValue("withoutChange"))
+	fmt.Println(r.FormValue("withBicycleDelivery"))
 
 	/*if TODO:feltétel? {
 
