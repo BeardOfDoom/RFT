7e8db9600f5b6651e69e39750ff486c6c913bd75
Birtalan Robert <birtalan_robert@gmail.hu>
2016-11-30 11:28:54 +0100

    date extraction

--- RFT/src/Handler/trainsAndTicketsHandler.go
+++ RFT/src/Handler/trainsAndTicketsHandler.go
@@ -8,10 +8,43 @@ import (
 	"strings"
 )
 
+func convertMonth(date string) string {
+	var month string
+	switch date {
+	case "Jan":
+		month = "01"
+	case "Feb":
+		month = "02"
+	case "Mar":
+		month = "03"
+	case "Apr":
+		month = "04"
+	case "May":
+		month = "05"
+	case "Jun":
+		month = "06"
+	case "Jul":
+		month = "07"
+	case "Aug":
+		month = "08"
+	case "Sep":
+		month = "09"
+	case "Oct":
+		month = "10"
+	case "Nov":
+		month = "11"
+	case "Dec":
+		month = "12"
+	}
+	return month
+}
+
 func SearchTimetable(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	//TODO: kitálni, hogy hogyan legyen lekezelve a discount és a pótjegy nélkül majd az ár kiszámításánál
 	//TODO: valamint a helyi közlekedés nélkül és a kerékpárszállítással is
+
+	//d tartalmazza a kedvezmenyt
 	discount := r.FormValue("discount")
 	if strings.HasSuffix(discount, ")") {
 		id := strings.LastIndex(discount, "(")
@@ -27,6 +60,13 @@ func SearchTimetable(w http.ResponseWriter, r *http.Request) {
 		d := "100"
 		fmt.Println(d)
 	}
+
+	date := r.FormValue("date")
+	year := date[11:15]
+	month := convertMonth(date[4:7])
+	day := date[8:10]
+	date = year + "-" + month + "-" + day
+	fmt.Println(date)
 
 	result := Service.SearchTimetable(r.FormValue("from"), r.FormValue("to"), r.FormValue("date"),
 		r.FormValue("discount"), r.FormValue("withoutExtraTicket"), r.FormValue("withoutLocalTransportation"),
