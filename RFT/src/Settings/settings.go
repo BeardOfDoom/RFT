f29f8a8a3911dc240ad75941492b44b3fcc21edf
Gencsi Mátyás <gencsimatyas@webshopexperts.hu>
2016-12-10 20:43:08 +0100

    no change

--- RFT/src/Settings/settings.go
+++ RFT/src/Settings/settings.go
@@ -1,9 +1,9 @@
 package Settings
 
 const (
-  DBHOST = "db4free.net"
-  DBUSERNAME = "rftvasut"
-  DBPASSWORD = "rftvasut"
-  DBNAME = "rftvasut"
+  DBHOST = "sql7.freemysqlhosting.net"
+  DBUSERNAME = "sql7147869"
+  DBPASSWORD = "97UtFvxYej"
+  DBNAME = "sql7147869"
   DBPORT = 3306
 )
