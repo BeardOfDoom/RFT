24c35cb5640d4c71e9a2091b8236b4abf9c3b3c5
Birtalan Robert <birtalan_robert@gmail.hu>
2016-12-12 14:38:22 +0100

    collecting, sending stations on the route

--- RFT/src/Handler/mapHandler.go
+++ RFT/src/Handler/mapHandler.go
@@ -1,6 +1,7 @@
 package Handler
 
 import (
+	"Service"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -9,14 +10,10 @@ import (
 func Map(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	fmt.Println("from: " + r.FormValue("from"))
-	fmt.Println("to: " + r.FormValue("to"))
-	fmt.Println("departure: " + r.FormValue("departure"))
-	fmt.Println("arrival: " + r.FormValue("arrival"))
-	fmt.Println("train: " + r.FormValue("train"))
-	fmt.Println("route: " + r.FormValue("route"))
+	result := Service.ListStationsByRouteID(r.FormValue("from"), r.FormValue("to"),
+													r.FormValue("departure"), r.FormValue("arrival"), r.FormValue("route"))
 
-	data := ""
+	fmt.Println(result)
 	t, _ := template.ParseFiles("View/Map/index.html", "View/Layout/main.html")
-	t.ExecuteTemplate(w, "layout", data)
+	t.ExecuteTemplate(w, "layout", result)
 }
--- RFT/src/Service/trainsAndTicketsService.go
+++ RFT/src/Service/trainsAndTicketsService.go
@@ -10,3 +10,9 @@ func SearchTimetable(from, to, date, discount string, potjegy, helyi, atszallas,
 	result := SQLAdapter.MysqlSearchTimetable(from, to, date, discount, potjegy, helyi, atszallas, kerekpar)
 	return result
 }
+
+func ListStationsByRouteID(from, to, departure, arrival, route_id string) Adapter.MapData {
+	SQLAdapter := Adapter.SQLFactory(Settings.DBUSERNAME, Settings.DBPASSWORD, Settings.DBHOST, Settings.DBNAME, Settings.DBPORT)
+	result := SQLAdapter.MysqlListStationsByRouteID(from, to, departure, arrival, route_id)
+	return result
+}
